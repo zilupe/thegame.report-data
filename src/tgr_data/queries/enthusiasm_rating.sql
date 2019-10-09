3b0ae46ab2f035b57f0900b8c0e327b03202ea02
Jazeps Basko <jazeps.basko@gmail.com>
2019-10-09 14:04:16 +0100

    enthusiasm rating query

--- src/tgr_data/queries/enthusiasm_rating.sql
+++ src/tgr_data/queries/enthusiasm_rating.sql
@@ -1,14 +1,29 @@
-SELECT * FROM
+SELECT
+    player_id,
+    team_id,
+    num_games,
+    printf("%.1f", avg_enthusiasm_rating) as "avg_enthusiasm_rating",
+    printf("%.1f", avg_pir) as "avg_pir",
+    printf("%.1f", avg_fga) as "avg_fga",
+    printf("%.1f", 100. * "fg%") as "fg%",
+    printf("%.1f", avg_3pa) as "avg_3pa",
+    printf("%.1f", 100. * "3p%") as "3p%",
+    printf("%.1f", avg_2pa) as "avg_2pa",
+    printf("%.1f", 100. * "2p%") as "2p%",
+    printf("%.1f", avg_fta) as "avg_fta",
+    printf("%.1f", 100. * "ft%") as "ft%",
+    printf("%.1f", avg_reb) as "avg_reb",
+    printf("%.1f", avg_ast) as "avg_ast",
+    printf("%.1f", avg_stl) as "avg_stl",
+    printf("%.1f", avg_tov) as "avg_tov",
+    printf("%.1f", avg_blk) as "avg_blk",
+    printf("%.1f", avg_pf) as "avg_pf"
+FROM
 (
 	SELECT
 
 		player_id,
 		team_id,
-
--- 		opponent_id,
-
-		case when opponent_id in ('burritos', 'lostangels', 'dropbears', 'rockets') then 'top4'
-	    else 'bottom4' end as 'opponent_strength',
 
 		count(*) as num_games,
 
@@ -66,28 +81,23 @@ SELECT * FROM
 		sum(pf) as "pf",
 		avg(pf) as "avg_pf",
 
-		sum(pts) + sum(reb) + sum(ast) + sum(stl) + sum(blk)
-		- ((sum(fta) - sum(ftm)) + (sum(fga) - sum(fgm)) + sum(tov) + sum(pf))
-		as "pir",
+		sum(pir) as "pir",
+		avg(pir) as "avg_pir",
 
-		1.0 * (sum(pts) + sum(reb) + sum(ast) + sum(stl) + sum(blk)
-		- ((sum(fta) - sum(ftm)) + (sum(fga) - sum(fgm)) + sum(tov) + sum(pf))) / count(*)
-		as "avg_pir"
+		sum(enthusiasm_rating) as "enthusiasm_rating",
+		avg(enthusiasm_rating) as "avg_enthusiasm_rating"
 
 	from "player_logs"
 
 	group by
 		player_id,
-		team_id,
-		opponent_strength
+		team_id
+
 
 ) aaa
 
-where
+where aaa.num_games >= 3
 
-aaa.opponent_strength = 'top4'
--- and aaa.num_games >= 2
-
-order by aaa."avg_pir" desc;
+order by aaa."avg_enthusiasm_rating" desc
 
 ;
